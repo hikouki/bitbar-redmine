1a1ee3dc780e706a02d68f742bf2c92b6ae85154
hikouki <ame.kh000@gmail.com>
2016-03-28 17:31:46 +0900

    Refactor code.

--- redmine-show-my-task.1m.rb
+++ redmine-show-my-task.1m.rb
@@ -26,11 +26,9 @@ redmine_url = ENV["REDMINE_URL"] || ''
 uri = URI.parse("#{redmine_url}/issues.json?key=#{token}&limit=100&status_id=open&assigned_to_id=me")
 
 begin
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true if(uri.scheme == 'https')
-  res = http.start {
+  res = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do | http |
     http.get(uri.request_uri)
-  }
+  end
 
   raise "error #{res.code} #{res.message}" if res.code != '200'
 
@@ -43,7 +41,12 @@ begin
       trackers: Hash.new do | h1, k1 |
         h1[k1] = {
           name: "tracker name.",
-          issues: Hash.new {| h2, k2 | h2[k2] = []}
+          statuses: Hash.new do | h2, k2 |
+            h2[k2] = {
+              name: "status name.",
+              issues: []
+            }
+          end
         }
       end
     }
@@ -51,15 +54,14 @@ begin
 
   issues.each do | v |
     project_id   = v[:project][:id]
-    project_name = v[:project][:name]
     status_id    = v[:status][:id]
     tracker_id   = v[:tracker][:id]
-    tracker_name = v[:tracker][:name]
     projects[project_id][:issues_count] += 1
     projects[project_id][:id] = project_id
-    projects[project_id][:name] = project_name
-    projects[project_id][:trackers][tracker_id][:name] = tracker_name
-    projects[project_id][:trackers][tracker_id][:issues][status_id].push(v)
+    projects[project_id][:name] = v[:project][:name]
+    projects[project_id][:trackers][tracker_id][:name] = v[:tracker][:name]
+    projects[project_id][:trackers][tracker_id][:statuses][status_id][:name] = v[:status][:name]
+    projects[project_id][:trackers][tracker_id][:statuses][status_id][:issues].push(v)
   end
 
   issue_total_count = result[:total_count] > 99 ? '99+' : result[:total_count]
@@ -72,10 +74,10 @@ begin
     puts "#{project[:name]}: #{project[:issues_count]} | size=11"
     project[:trackers].each do | _, tracker |
       puts "➠ #{tracker[:name]} | color=#33BFDB size=11"
-      tracker[:issues].each do | _, status |
-        puts "[#{status.first[:status][:name]}] | color=#58BE89 size=11"
-        status.each do | issue |
-          prefix = status.last == issue ? "└" : "├"
+      tracker[:statuses].each do | _, status |
+        puts "[#{status[:name]}] | color=#58BE89 size=11"
+        status[:issues].each do | issue |
+          prefix = status[:issues].last == issue ? "└" : "├"
           puts "#{prefix} ##{issue[:id]} #{issue[:subject]} | color=black href=#{redmine_url}/issues/#{issue[:id]} size=11"
         end
       end
